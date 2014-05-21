31b0a52286f53fb3660aa539512ad94f603a7638
Nathan Nichols <broinjc@gmail.com>
2014-05-21 17:12:32 +0000

    yo

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 
-// How you import js into js!
 var hapi = require('hapi');
-var test = require('./test')
 var ansi = require('simple-ansi');
 var redis = require('redis');
 var options = {
@@ -14,29 +12,31 @@ var options = {
 };
 var server_port = 80;
 var server = new hapi.Server('0.0.0.0', server_port, options);
-
 var redisClient  = redis.createClient(6379, "localhost");
-
 var max_downloads = 5
 
+
+// CONTROLLERS
 function homepage(request, reply) {
-	reply("Hi there! Nothing to see here, move along...");
+	reply.view("index", {
+		ip:request.raw.req.connection.remoteAddress
+	});
 }
 
 function downloadable(request, reply) {
-	// Check value of key in redis
-	// If value < max_num_of_downloads_allowed proceed to access download
-	// If value > max_num_of_downloads_allowed DEL key:value store
-	// Hard Code
-
 	redisClient.get(request.params.token,function(err, tokenval) {
   
 		if(tokenval && tokenval < max_downloads){
 			
-			reply.file('./deliverables/letter.zip')
+			var options = {
+				filename:'A-Letter.zip',
+			}
+			reply.file('./deliverables/letter.zip', options)
+			// NOSQL!
 			redisClient.incr(request.params.token);
+
 		} else {
-			reply.view("index", {
+			reply.view("downloads", {
 				max_downloads: max_downloads,
 				css_status: 'red',
 				token: request.params.token,
@@ -46,17 +46,20 @@ function downloadable(request, reply) {
 
 	});
 }
+
+// ROUTES
 server.route([
   {method: 'GET', path: '/', handler: homepage},
   {method: 'GET', path: '/d/{token}', handler: downloadable},
 
 //{method: 'POST', path:'/d/', handler: downloadcreator}
-	{method: 'GET', path: '/vendor/{jslib}', handler: { directory: { path: "./vendor/"} }},
+	{ method: 'GET', path: '/bower_components/{jslib}', handler: { directory: { path: "./bower_components/"} }},
   { method: 'GET', path: '/scripts/{js}', handler: { directory: { path: "./scripts/"} }},
   { method: 'GET', path: '/css/{cssfile}', handler: { directory: { path: "./css/"} }},
   { method: 'GET', path: '/fonts/{cssfile}', handler: { directory: { path: "./fonts/"} }},
 ]);
 
+// SERVER
 server.start( function() {
   now = new Date();  
   console.log('['+ ansi.green + 'INFO' + ansi.reset + ']' + ' ' + now + ' Starting HAPI server at port: ' + ansi.blue + server_port + ansi.reset);
--- test.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {};
-
-module.exports.testval = 5;
-module.exports.action= function() {
-	console.log("yo");
-}
