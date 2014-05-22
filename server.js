fd97f79ace482a5212796480e3a4e7b007f8d2e7
Nathan Nichols <broinjc@gmail.com>
2014-05-22 17:12:02 +0000

    todo list mockup

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-
 var hapi = require('hapi');
-var ansi = require('simple-ansi');
-var redis = require('redis');
-var options = {
+var hapi_options = {
 	views: {
     engines: { html: 'handlebars'},
     path: __dirname + '/views',
@@ -10,10 +7,24 @@ var options = {
     helpersPath: __dirname + '/views/helpers'
   }
 };
+var ansi = require('simple-ansi');
+var redis = require('redis');
 var server_port = 80;
-var server = new hapi.Server('0.0.0.0', server_port, options);
+var server = new hapi.Server('0.0.0.0', server_port, hapi_options);
 var redisClient  = redis.createClient(6379, "localhost");
+
+// APP
 var max_downloads = 5
+
+// HELPER FUNCTIONS
+function slug(len){
+	o = 'abcdefghejklmnopqrstuvwxyz1234567890';
+	r = '';
+	for(var i = 0; i < len; i++){
+		r = r + o[Math.floor(Math.random()*100)%o.length]
+	}
+	return r;
+}
 
 
 // CONTROLLERS
@@ -25,17 +36,17 @@ function homepage(request, reply) {
 
 function downloadable(request, reply) {
 	redisClient.get(request.params.token,function(err, tokenval) {
-  
+
+  	// Does the key exist and have they exceeded max downloads? 
 		if(tokenval && tokenval < max_downloads){
-			
 			var options = {
 				filename:'A-Letter.zip',
 			}
 			reply.file('./deliverables/letter.zip', options)
-			// NOSQL!
 			redisClient.incr(request.params.token);
-
-		} else {
+		} 
+		else 
+		{
 			reply.view("downloads", {
 				max_downloads: max_downloads,
 				css_status: 'red',
@@ -43,17 +54,16 @@ function downloadable(request, reply) {
 				tokenval: tokenval
 			});
 		}
-
 	});
 }
 
 // ROUTES
 server.route([
+//  {method: 'GET', path: '/b/{path*}', handler: { directory: { path: './bower_components', listing: false } }},
   {method: 'GET', path: '/', handler: homepage},
   {method: 'GET', path: '/d/{token}', handler: downloadable},
 
-//{method: 'POST', path:'/d/', handler: downloadcreator}
-	{ method: 'GET', path: '/bower_components/{jslib}', handler: { directory: { path: "./bower_components/"} }},
+//  { method: 'POST', path:'/d/', handler: downloadcreator},
   { method: 'GET', path: '/scripts/{js}', handler: { directory: { path: "./scripts/"} }},
   { method: 'GET', path: '/css/{cssfile}', handler: { directory: { path: "./css/"} }},
   { method: 'GET', path: '/fonts/{cssfile}', handler: { directory: { path: "./fonts/"} }},
